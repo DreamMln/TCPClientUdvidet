452d051a0498584e5fde5095de03a15f5e693486
DreamMln <maix2374@edu.zealand.dk>
2022-08-20 13:13:37 +0200

    Add project files.

---
+++ TcpClient29aug/Program.cs
@@ -0,0 +1,50 @@
+﻿using DiceLib29aug;
+using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text.Json;
+
+namespace TcpClient29aug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tcp Client!");
+
+            //kommunikere med serveren, importer dll fil, udfra den protokol er 
+            //defineret i serveren, 
+            //værdier er hardcodede...
+            
+            ////////////////////////////////////////
+            //brugeren der skal indtaste værdierne//
+            ////////////////////////////////////////
+
+            TcpClient socket = new TcpClient("localhost", 7);
+            //send en strøm af data
+            NetworkStream ns = socket.GetStream();
+            //læs stream
+            StreamReader reader = new StreamReader(ns);
+            //skriv stream
+            StreamWriter writer = new StreamWriter(ns);
+
+            //objekt oprettes
+            //DiceResult Dice = new DiceResult(5, "Lone");
+            //seralisere obj.
+            //string messageSerializeDice = JsonSerializer.Serialize(Dice);
+
+            //slut client - skriver i console
+            string messageReadline = Console.ReadLine();
+            //sender det til serveren
+            writer.WriteLine(messageReadline);
+            //ryd cache - skyl ud!
+            writer.Flush();
+        
+                //det modtages som en string
+                    string replyFromServer = reader.ReadLine();
+
+                    Console.WriteLine("Server sent this reply: " + replyFromServer);
+
+        }
+    }
+}
